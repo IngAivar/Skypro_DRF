b495c47a39a278beeb0d67070650cbd6f7e20f29
MiniGan067 <MiniGan067@yandex.ru>
2023-07-18 23:14:00 +0300

    HW 25.1 (1, 2, 3)

--- app_image/admin.py
+++ app_image/admin.py
@@ -1,21 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import CourseImage, LessonImage, UserImage
-
-
-@admin.register(CourseImage)
-class CourseImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image']
-    list_display_links = ['image']
+from .models import CustomUser, Payment
 
 
-@admin.register(LessonImage)
-class LessonImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image']
-    list_display_links = ['image']
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """
+    Класс, описывающий модель CustomUser в административном интерфейсе.
+    """
+
+    model = CustomUser
+    list_display = ['pk', 'email', 'first_name', 'last_name', 'is_active']
+    list_display_links = ['pk', 'email']
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('last_login', 'date_joined')
+
+    def get_fieldsets(self, request, obj=None):
+        """
+        Возвращает кортеж кортежей, определяющих расположение полей на форме.
+        """
+        if not obj:
+            fieldsets = (
+                (None, {'fields': ('email', 'password1', 'password2')}),
+                ('Персональная информация', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+            )
+        else:
+            fieldsets = (
+                (None, {'fields': ('email',)}),
+                ('Персональная информация', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+                ('Разрешения', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+                ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+            )
+        return fieldsets
 
 
-@admin.register(UserImage)
-class UserImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image']
-    list_display_links = ['image']
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+
+    model = Payment
+    list_display = ['pk', 'user', 'payment_date', 'paid_course', 'paid_lesson', 'amount', 'payment_method']
+    list_display_links = ['pk']
